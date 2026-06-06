c49853ba7eb4bef2fc3f965c13ccd8fd2687687e
arpitmet2 <arpitmet.2@gmail.com>
2026-06-06 10:33:24 +0530

    streamlit home screen

---
+++ src/screens/student_screen.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def student_screen():
+
+    st.title("Welcome to the Student Screen")
+    st.write("This is where students can view their courses and grades.")
---
+++ src/screens/teacher_screen.py
@@ -0,0 +1,5 @@
+import streamlit as st
+def teacher_screen():
+
+    st.title("Welcome to the Teacher Screen")
+    st.write("This is where teachers can manage their courses and students.")
